0584c3734826257d0f6013f94cd7a0affc0dd5f9
jitheshkumar2401059 <jitheshkumar.2401059@srec.ac.in>
2026-08-29 09:14:00 +0530

    Add Pizza Bill Counter project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+//        pizza basepizza=new pizza(true);
+//        basepizza.addextracheese();
+//        basepizza.addextratopings();
+//        basepizza.takeaway();
+//        basepizza.getbill();
+        deluxpizza dp=new deluxpizza(true);
+        dp.addextracheese();
+        dp.addextratopings();
+        dp.getbill();
+    }
+}
